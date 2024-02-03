890d381e8d5517baeb00963bd983627d8f030b24
Leonel Molina <leonel.molina@softwarehouse.com.ec>
2024-02-02 19:49:40 -0500

    new endpoint soap2

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ async def root():
     return "Hola"
 
 @app.get("/soap")
-async def root():
-    autorization = "Bearer eyJraWQiOiJrZzplMGRlNmFiZC1jY2NlLTQyYzEtYjFlNS05ZDkwNjdhMmRkMGMiLCJhbGciOiJSUzI1NiJ9.eyJTZXJ2aWNlQWNjb3VudCI6Ik1GNUEyV0w1N0xTQjVQUEVfREVNI1ljanJJOVNUQnhxNUEyQ0dwZEtUQ214Nl9Db0FiNG1KYTBLZkdBek5yR3JRQ18zOGh3by1DT01OeW5YQUI4bmxNd2x0bVpadEVFQi1KWDVNNk43cEVnIiwiVGVuYW50IjoiTUY1QTJXTDU3TFNCNVBQRV9ERU0iLCJJZGVudGl0eTIiOiI0YmVhYjNmMS02MzRjLTRlZjAtOWU3My0xYjIxMTY1OTFlYzUiLCJFbmZvcmNlU2NvcGVzRm9yQ2xpZW50IjoiMCIsImdyYW50X2lkIjoiYjI1Yzc4ZjktODgwZC00MzBiLWJjN2EtZGM3NzhmYmI2ZDQzIiwiSW5mb3JTVFNJc3N1ZWRUeXBlIjoiQVMiLCJjbGllbnRfaWQiOiJNRjVBMldMNTdMU0I1UFBFX0RFTX40bS1Jb1lTb09HTFFVck8wa0lZUE80VGRzaE9GUHU3QVVfSWo5UjlqdkhJIiwianRpIjoiYmE4MmU5OTYtZjlmZi00MmYxLTgzOGMtZTNhNWU5NTA0MTE5IiwiaWF0IjoxNzA2OTE5MDk2LCJuYmYiOjE3MDY5MTkwOTYsImV4cCI6MTcwNjkyNjI5NiwiaXNzIjoiaHR0cHM6Ly9taW5nbGUtc3NvLmluZm9yY2xvdWRzdWl0ZS5jb206NDQzIiwiYXVkIjoiaHR0cHM6Ly9taW5nbGUtaW9uYXBpLmluZm9yY2xvdWRzdWl0ZS5jb20ifQ.RInnE_ykYwmeNCs7NQYRKrSNeP1c2Cqo1K31L_kVEY2XzAbT-XO6dWlcxPDRACdNyyXVC4lW2HIhMoiczawQIJW6nKQECgBZNCxho00YItlQF0tTuwx2VUoCFeJwpy9il6IX__6QXyDunoXGPFbO-umIUbLtBdcr3_HXQtZZ3TNdPEtg50uCXGUc3MZ9dFpMZmvabt8dOg7-uE6SENEPmdFF9i2ToM2zDlo5y09ASyJjmJvhr-2fK0MEvkGDyMDQH_pTuyhRvInOCZSV0hK-kGZRc-6LdGbe-rR-Jpj5FPea8DFB6uiYofdjxWE_p1ruKTJZo2PbiYO-LYU4DqmjmQ"
-    req_headers = {"content-type": "text/xml; charset=utf-8", "authorization": autorization}
+async def root(bearerToken: str):
+    req_headers = {"content-type": "text/xml; charset=utf-8", "authorization": "Bearer " + bearerToken}
     req_body =  "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
     req_body += "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:pur=\"http://www.infor.com/businessinterface/Contact_v3\" >"
     req_body += "<soapenv:Header></soapenv:Header>"
@@ -33,7 +32,6 @@ async def root():
     req_body += "</soapenv:Body>"
     req_body += "</soapenv:Envelope>"
     response = requests.post(
-        # "lid://infor.ln.ln01",
         "https://mingle-ionapi.inforcloudsuite.com/MF5A2WL57LSB5PPE_DEM/LN/c4ws/services/Contact_v3",
         data=req_body,
         headers=req_headers
